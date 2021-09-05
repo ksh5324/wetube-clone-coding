1d464f17a502ed006ffebfb4f0bbb22f7aa7aadd
강성훈 <yolly050101@naver.com>
2021-09-05 15:04:05 +0900

    request, response, middleware

--- src/server.js
+++ src/server.js
@@ -4,7 +4,31 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", () => console.log("Somebody is trying to go home."));
+const loggerMiddleware = (req, res, next) => {
+  console.log(` ${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</hr>");
+  }
+  next();
+};
+
+const handleHome = (req, res, next) => {
+  return res.send("I Love middlewares");
+};
+
+const handleLogin = (req, res) => {
+  return res.send({ message: "Login here" });
+};
+
+app.use(loggerMiddleware);
+app.use(privateMiddleware);
+app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
