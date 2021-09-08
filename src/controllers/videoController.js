4b0bb140081bb91f94ff2c866cd8c8aad6890917
강성훈 <yolly050101@naver.com>
2021-09-08 16:45:50 +0900

    TEMPLATES

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,59 @@
+const fakeUser = {
+  username: "Nico",
+  loggedIn: true,
+};
+
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  return res.render("home");
+  return res.render("home", {
+    pageTitle: "Home",
+    videos: videos,
+    fakeUser: fakeUser,
+  });
 };
 
 export const see = (req, res) => {
   console.log(req.params);
-  return res.send("see");
+  return res.render("watch", {
+    pageTitle: "Home",
+    videos: videos,
+    fakeUser: fakeUser,
+  });
 };
 
 export const edit = (req, res) => {
   console.log(req.params);
-  return res.send("Edit");
+  return res.render("edit", {
+    pageTitle: "Home",
+    videos: videos,
+    fakeUser: fakeUser,
+  });
 };
 
 export const search = (req, res) => {
