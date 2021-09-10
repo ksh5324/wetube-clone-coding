c4d5e23ec029ec21bd7e28ceadb0e7e7f681d85c
강성훈 <yolly050101@naver.com>
2021-09-11 01:24:08 +0900

    arrayDB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -45,7 +45,29 @@ export const getEdit = (req, res) => {
 };
 
 export const postEdit = (req, res) => {
-  return res.redirect();
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Videos" });
+};
+
+export const postUpload = (req, res) => {
+  // here we will add a video to the videos array.
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 0,
+    comments: 0,
+    createdAt: "just now",
+    views: 0,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
 };
 
 // export const search = (req, res) => {
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,16 +1,17 @@
 import express from "express";
 import {
-  upload,
-  deleteVideo,
   watch,
   getEdit,
   postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 // videoRouter.get(":id(\\d+)/delete", deleteVideo);
 // videoRouter.get("/upload", upload);
 
